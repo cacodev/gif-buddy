1cccfa463be0ace132704617136d03a4e48fe91c
Casey Collins <casey.collins@dcsg.com>
2018-10-17 11:06:13 -0400

    adding rabbit mq listener

--- index.js
+++ index.js
@@ -3,15 +3,44 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
+const amqp = require('amqplib/callback_api');
+
+const newBotBuddyQ = 'addBotBuddy';
+const newBotBuddyGifQ = 'addBotBuddyGif';
 
 app.use(express.static(__dirname + '/public'));
 
-function onConnection(socket){
-  socket.on('addBuddy', (data) => {
-      socket.broadcast.emit('addBuddy', data);
-  });
-  socket.on('addBuddyGif', (data) => {
-    socket.broadcast.emit('addBuddyGif', data);
+function onConnection(socket) {
+    socket.on('addBuddy', (data) => {
+        socket.broadcast.emit('addBuddy', data);
+    });
+    socket.on('addBuddyGif', (data) => {
+        socket.broadcast.emit('addBuddyGif', data);
+    });
+
+    amqp.connect('amqp://localhost', function (err, conn) {
+    conn.createChannel(function (err, ch) {
+        ch.assertQueue(newBotBuddyQ, {
+            durable: false
+        });
+        ch.assertQueue(newBotBuddyGifQ, {
+            durable: false
+        });
+
+        ch.consume(newBotBuddyQ, function (msg) {
+            console.log(JSON.parse(msg.content));
+            socket.broadcast.emit('addBuddy', JSON.parse(msg.content));
+        }, {
+            noAck: true
+        });
+        
+        ch.consume(newBotBuddyGifQ, function (msg) {
+            console.log(JSON.parse(msg.content));
+            socket.broadcast.emit('addBuddyGif', JSON.parse(msg.content));
+        }, {
+            noAck: true
+        });
+    });
 });
 }
 
